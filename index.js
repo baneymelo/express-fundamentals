92e50a5b1799024e245055bd5e186c126fc8b19a
baney <baneymelo@gmail.com>
2020-09-03 14:40:17 -0500

    03. Middelwares

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 
-//02. Routing 
+//03. Middelwares
 
 const express = require('express');
+const morgan = require('morgan');
 const app = express();
 
+
 app.use(express.json())
+app.use(morgan('tiny'));
+
 
 app.get('/user', (req, res) =>{
     res.json({
@@ -19,7 +23,7 @@ app.post('/user/:id', (req, res) =>{
     res.send("Got a POST request")
 })
 
-app.put('/user/:userId', (req, res) =>{
+app.put('/user/:userId', (req, res) =>{  
     console.log(req.body);
     res.send(`User ${req.params.userId} updated`)
 })
@@ -28,6 +32,8 @@ app.delete('/user/:userId', (req, res) =>{
     res.send(`User ${req.params.userId} deleted`)
 })
 
+
+app.use(express.static('public'));
 
 app.listen(3000, () =>{
     console.log("Server on port 3000");
