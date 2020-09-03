a360b18e04ca66ec10fff62135d7013138eed21f
baney <baneymelo@gmail.com>
2020-09-02 21:47:01 -0500

    02. Routing

--- index.js
+++ index.js
@@ -1,13 +1,34 @@
 
-//01. Start a server 
+//02. Routing 
 
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) =>{
-    res.send("Hello World")
+app.use(express.json())
+
+app.get('/user', (req, res) =>{
+    res.json({
+        username:"Carlos",
+        lastname:"Hurtado"
+    })
 })
 
+app.post('/user/:id', (req, res) =>{
+    console.log(req.body);
+    console.log(req.params);
+    res.send("Got a POST request")
+})
+
+app.put('/user/:userId', (req, res) =>{
+    console.log(req.body);
+    res.send(`User ${req.params.userId} updated`)
+})
+
+app.delete('/user/:userId', (req, res) =>{
+    res.send(`User ${req.params.userId} deleted`)
+})
+
+
 app.listen(3000, () =>{
-    console.log("Server on port 3000!");
+    console.log("Server on port 3000");
 })
