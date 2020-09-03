f551c401e0b76fa76c7cbbcbbca65d3a7ed9c31d
baney <baneymelo@gmail.com>
2020-09-02 19:32:42 -0500

    01. Start a server

---
+++ index.js
@@ -0,0 +1,16 @@
+
+//01. Start a server 
+
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) =>{
+    res.send("Hello World")
+})
+
+app.listen(3000, () =>{
+    console.log("Server on port 3000!");
+})
+
+
+
